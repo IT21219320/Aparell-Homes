70ae018aae8cdae930cc3ffa11319e66eeb60b0c
IT21219320 <it21219320@my.sliit.lk>
2022-10-13 11:28:11 +0530

    js edit

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,15 @@
 //Change theme
+var theme = localStorage.getItem("Light");
+light(theme);
 function light(val){
-    if(val == 'ON'){
+    if(val == 'OFF'){
         document.getElementById("stylesheet").href = "style/darkstyle.css";
         document.getElementById("logo").src = "images/Logo(dark).png";
+        localStorage.setItem("Light","OFF");
     }
-    else if(val == 'OFF'){
+    else if(val == 'ON'){
         document.getElementById("stylesheet").href = "style/lightstyle.css";
         document.getElementById("logo").src = "images/Logo(light).png";
+        localStorage.setItem("Light","ON");
     }
 }
